8b5394e9285e7efa59b7c5dae1b1fd0679e0fb86
Boris Feld <lothiraldan@gmail.com>
2019-07-05 23:39:50 +0200

    Add the actual integration tests script

---
+++ scripts/integration_test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -euox pipefail
+
+# Script that run the whole data pipeline the fastest possible to validate
+# that every part is working with the others
+
+# Supposed to be run from the repository root directory
+
+cd http_service/models/;
+
+# Remove the model
+rm defectenhancementtaskmodel* || true;
+rm backout* || true;
+
+# First retrieve a subset of bugs data
+# TODO: Let the script download the previous DB as it should be pretty fast?
+bugbug-data-bugzilla --limit 100
+
+# Then retrieve a subset of commit data
+mkdir -p cache
+bugbug-data-commits --limit 100 cache
+
+# Then train a bug model
+bugbug-train --limit 200 --no-download defectenhancementtask
+
+# Then train a commit model
+bugbug-train --limit 30000 --no-download backout
+
+# Then build docker images
+cd ../..;
+docker-compose build --pull bugbug-base
+
+cd http_service/models;
+docker-compose build --build-arg CHECK_MODELS=0
